6989699de53662b434ef29c878bbf6769c472a51
Kristina Uldina <uldinakris@gmail.com>
2021-09-20 11:37:43 +0200

    Number of bonus miles from one ticket

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        int ticketCost = 502;
+        int oneMileBonus = 20;
+        int bonusMiles = ticketCost / oneMileBonus;
+        System.out.println(bonusMiles);
+        
+    }
 }
+
